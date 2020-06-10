fb5cb56c2531f9da7cc652e475de3161f7ebabb1
prajput <>
2020-06-10 08:37:59 -0400

    {/src/main/java/com/foo/test_record_vector/PR_sub_rec_new.java}

--- src/main/java/com/foo/test_record_vector/PR_sub_rec_new.java
+++ src/main/java/com/foo/test_record_vector/PR_sub_rec_new.java
@@ -4,24 +4,19 @@ public class PR_sub_rec_new implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
-	
-
 	public PR_sub_rec_new() {
 	}
 
 	private java.lang.String data_country;
 	private java.lang.String data_business;
-	private bus_dim bus_dim_field;
 	private java.util.List<com.foo.test_record_vector.bus_dim> temp_fld;
-	private java.util.List<java.lang.String> list_fld_test;
-
-	 public java.lang.String sub_data_fetch() {
-	     com.foo.test_record_vector.bus_dim bus_dim_nested_only;
-	     bus_dim_nested_only=temp_fld.get(1);
-	     String get_LEVEL=bus_dim_nested_only.getDim_name();
-	     return get_LEVEL;
-	     //return 'SUCC';
-	 }
+	public java.lang.String sub_data_fetch() {
+		com.foo.test_record_vector.bus_dim bus_dim_nested_only;
+		bus_dim_nested_only = temp_fld.get(1);
+		String get_LEVEL = bus_dim_nested_only.getDim_name();
+		return get_LEVEL;
+		// return 'SUCC';
+	}
 	public java.lang.String getData_country() {
 		return this.data_country;
 	}
@@ -38,15 +33,6 @@ public class PR_sub_rec_new implements java.io.Serializable {
 		this.data_business = data_business;
 	}
 
-	public com.foo.test_record_vector.bus_dim getbus_dim_field() {
-		return this.bus_dim_field;
-	}
-
-	public void setbus_dim_field(
-			com.foo.test_record_vector.bus_dim bus_dim_field) {
-		this.bus_dim_field = bus_dim_field;
-	}
-
 	public java.util.List<com.foo.test_record_vector.bus_dim> getTemp_fld() {
 		return this.temp_fld;
 	}
@@ -55,14 +41,5 @@ public class PR_sub_rec_new implements java.io.Serializable {
 			java.util.List<com.foo.test_record_vector.bus_dim> temp_fld) {
 		this.temp_fld = temp_fld;
 	}
-
-	public java.util.List<java.lang.String> getList_fld_test() {
-		return this.list_fld_test;
-	}
-
-	public void setList_fld_test(java.util.List<java.lang.String> list_fld_test) {
-		this.list_fld_test = list_fld_test;
-	}
-
 
 }
