8843f6c6b15085ef4154674c8e87858a7a31db13
prajput <>
2020-06-11 02:26:08 -0400

    {/src/main/java/com/foo/test_record_vector/PR_sub_rec_new.java}

--- src/main/java/com/foo/test_record_vector/PR_sub_rec_new.java
+++ src/main/java/com/foo/test_record_vector/PR_sub_rec_new.java
@@ -4,15 +4,19 @@ public class PR_sub_rec_new implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
+
+
 	public PR_sub_rec_new() {
 	}
 
 	private java.lang.String data_country;
 	private java.lang.String data_business;
-	private java.util.List<com.foo.test_record_vector.bus_dim> temp_fld;
+    private java.util.List<com.foo.test_record_vector.bus_dim> bus_dim_list_field;
+	private com.foo.test_record_vector.bus_dim bus_dim_simple_fld;
+	
 	public java.lang.String sub_data_fetch() {
 		com.foo.test_record_vector.bus_dim bus_dim_nested_only;
-		bus_dim_nested_only = temp_fld.get(1);
+		bus_dim_nested_only = bus_dim_list_field.get(1);
 		String get_LEVEL = bus_dim_nested_only.getDim_name();
 		return get_LEVEL;
 		// return 'SUCC';
@@ -33,13 +37,19 @@ public class PR_sub_rec_new implements java.io.Serializable {
 		this.data_business = data_business;
 	}
 
-	public java.util.List<com.foo.test_record_vector.bus_dim> getTemp_fld() {
-		return this.temp_fld;
+	public java.util.List<com.foo.test_record_vector.bus_dim> getBus_dim_list_field() {
+		return this.bus_dim_list_field;
 	}
-
-	public void setTemp_fld(
-			java.util.List<com.foo.test_record_vector.bus_dim> temp_fld) {
-		this.temp_fld = temp_fld;
+	public void setBus_dim_list_field(
+			java.util.List<com.foo.test_record_vector.bus_dim> bus_dim_list_field) {
+		this.bus_dim_list_field = bus_dim_list_field;
+	}
+	public com.foo.test_record_vector.bus_dim getBus_dim_simple_fld() {
+		return this.bus_dim_simple_fld;
+	}
+	public void setBus_dim_simple_fld(
+			com.foo.test_record_vector.bus_dim bus_dim_simple_fld) {
+		this.bus_dim_simple_fld = bus_dim_simple_fld;
 	}
 
 }
